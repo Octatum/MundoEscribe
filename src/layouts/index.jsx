d7be53f9eadfad8bd8b829ecdcbeddd44ca19880
Roberto Ruiz <rroberto.rruiz@gmail.com>
2018-08-20 16:24:47 -0500

    Finished initial project setup

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,31 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import { ThemeProvider } from 'styled-components';
 
-import Header from '../components/header'
 import './index.css'
+import { globalTheme } from './theme';
 
 const Layout = ({ children, data }) => (
-  <div>
+  <ThemeProvider theme={globalTheme}>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
+        { name: 'description', content: 'El mundo escribe' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
-  </div>
+  </ThemeProvider>
 )
 
 Layout.propTypes = {
---
+++ src/layouts/theme.js
@@ -0,0 +1,13 @@
+export const globalTheme = {
+  color: {
+    white: 'white',
+    black: '#515362',
+    orange: 'hsl(21, 100%, 75%)',
+    darkOrange: 'hsl(21, 100%, 65%)',
+    gray: '#EFEFEF'
+  },
+  fonts: {
+    main: '',
+    secondary: ''
+  }
+};
