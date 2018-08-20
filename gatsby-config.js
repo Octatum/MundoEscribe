a52da74121072a19b6388d1944707c2a9968cc4d
Roberto Ruiz <rroberto.rruiz@gmail.com>
2018-08-20 16:15:14 -0500

    Added project plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'El Mundo Escribe',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-styled-components',
+  ],
 }
