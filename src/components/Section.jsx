2cfb880da4eadbe3a7dba887aaa88d6d40396bb6
Daniel Hernandez <tamdannyhdz@gmail.com>
2018-09-11 12:45:51 -0500

    Add responsiveness to Section component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
+import breakpoints from '../utils/breakpoints';
 
 const Section = styled.section`
   display: flex;
   flex: 1;
-  margin: 0 ${({fluid}) => fluid ? 0 : 7.3}em;
+  margin: 0 ${({ fluid }) => fluid ? 0 : 7.3}em;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    margin: 0 ${({ fluid }) => fluid ? 0 : '10%'};
+  }
 `;
 
 export default Section;
