1424032e3e6bd99e63e3a44f312f054faf2875ef
Daniel Hernandez <tamdannyhdz@gmail.com>
2018-09-19 16:46:20 -0500

    Add responsiveness to Volunteers modal

--- src/components/HowToHelp/Volunteers.jsx
+++ src/components/HowToHelp/Volunteers.jsx
@@ -31,12 +31,39 @@ const Square = styled.div`
   width: 10em;
   text-align: center;
   background: ${props => props.theme.color.lightBlue};
+  color: ${props => props.theme.color.white};
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    height: 6em;
+    width: 6em;
+  }
 `
 
 const SquarePicture = styled.img`
   max-height: 4em;
   max-width: 6em;
   padding: 1.5em;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    max-height: 2em;
+    max-width: 3em;
+    padding: 1em;
+  }
+`
+
+const Arrow = styled.i`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: ${props => !props.right ? '-1em' : 'initial'};
+  right: ${props => props.right ? '-1em' : 'initial'};
+  font-size: 3em;
+  color: ${props => props.theme.color.black};
+  cursor: pointer;
+
+  @media screen and (min-width: ${breakpoints.medium}) {
+    display: none !important;
+  }
 `
 
 const SquareText = styled.p`
@@ -44,17 +71,21 @@ const SquareText = styled.p`
   bottom: 0.5em;
   width: 100%;
   font-size: 1.3em;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    font-size: 1em;
+  }
 `
 
 const FlexContainer = styled.div`
   display: flex;
-  flex: 1;
   align-self: flex-start;
 `
 
 const Description = styled.div`
   flex: 1;
   padding: 0 3em;
+  color: ${props => props.theme.color.white};
 
   li {
     padding: 0.25em 0;
@@ -68,6 +99,21 @@ const Description = styled.div`
     margin-left: -1em;
     color: ${props => props.theme.color.lightBlue};
   }
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    width: 100%;
+    box-sizing: border-box;
+    padding: 1em 7% 8em;
+    margin: 2em 0;
+    background: url(${backgroundImage}) no-repeat center bottom;
+    background-size: cover;
+    max-height: 400px;
+
+    li::before {
+      width: 1.5em;
+      margin-left: -1.5em;
+    }
+  }
 `
 
 const Logo = styled.div`
@@ -79,13 +125,19 @@ const Logo = styled.div`
   img {
     width: 40%;
   }
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    display: none;
+  }
 `
 
-const Volunteers = () => (
+const Volunteers = (props) => (
   <Container>
     <Square>
+      <Arrow className="fas fa-angle-left" onClick={() => props.changeModal(false)}/>
       <SquarePicture src={icon}/>
       <SquareText>Voluntarios</SquareText>
+      <Arrow className="fas fa-angle-right" onClick={() => props.changeModal(true)} right/>
     </Square>
     <FlexContainer>
       <Description>
