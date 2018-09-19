315e73e4d2a2d69bb85a4c1b6c3fb703613f6f9b
Daniel Hernandez <tamdannyhdz@gmail.com>
2018-09-19 17:31:55 -0500

    Add responsiveness to Products modal

--- src/components/HowToHelp/Products.jsx
+++ src/components/HowToHelp/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import breakpoints from '../../utils/breakpoints';
 import icon from './assets/products-logo.svg';
 import logo from './../assets/logo.svg';
 import diary from './assets/products-diary.png';
@@ -28,6 +29,71 @@ const Container = styled.div`
   padding: 1em;
   background: #666666;
   color: ${props => props.theme.color.white};
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    flex-direction: column;
+    align-items: center;
+    padding: 5em 0 0;
+    background: ${props => props.theme.color.white};
+    color: ${props => props.theme.color.black};
+  }
+`
+
+const Square = styled.div`
+  min-height: 10em;
+  width: 10em;
+  position: absolute;
+  top: 1em;
+  right: 1em;
+  text-align: center;
+  background: ${props => props.theme.color.lightBlue};
+  color: ${props => props.theme.color.white};
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    position: relative;
+    top: 0;
+    right: 0;
+    min-height: 6em;
+    width: 6em;
+  }
+`
+
+const SquarePicture = styled.img`
+  max-height: 4em;
+  max-width: 6em;
+  padding: 1.5em;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    max-height: 2em;
+    max-width: 3em;
+    padding: 1em;
+  }
+`
+
+const Arrow = styled.i`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: ${props => !props.right ? '-1em' : 'initial'};
+  right: ${props => props.right ? '-1em' : 'initial'};
+  font-size: 3em;
+  color: ${props => props.theme.color.black};
+  cursor: pointer;
+
+  @media screen and (min-width: ${breakpoints.medium}) {
+    display: none !important;
+  }
+`
+
+const SquareText = styled.p`
+  position: absolute;
+  bottom: 0.5em;
+  width: 100%;
+  font-size: 1.3em;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    font-size: 1em;
+  }
 `
 
 const PhotoGrid = styled.div`
@@ -36,6 +102,14 @@ const PhotoGrid = styled.div`
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 1em 1em;
   width: 50%;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    position: relative;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 2em;
+    grid-gap: 0.5em 0.5em;
+  }
 `
 
 const Photo = styled.div`
@@ -57,6 +131,10 @@ const OpaqueDiv = styled.div`
   ${Photo}:hover & {
     opacity: 1;
   }
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    padding: 3em 0;
+  }
 `
 
 const PhotoName = styled.div`
@@ -75,41 +153,37 @@ const PhotoName = styled.div`
 const RightContainer = styled.div`
   flex: 1;
   display: flex;
+  font-size: 1.2em;
   flex-direction: column;
-`
 
-const Square = styled.div`
-  position: relative;
-  min-height: 10em;
-  width: 10em;
-  align-self: flex-end;
-  text-align: center;
-  background: ${props => props.theme.color.lightBlue};
-`
-
-const SquarePicture = styled.img`
-  max-height: 4em;
-  max-width: 6em;
-  padding: 1.5em;
-`
-
-const SquareText = styled.p`
-  position: absolute;
-  bottom: 0.5em;
-  width: 100%;
-  font-size: 1.3em;
+  @media screen and (max-width: ${breakpoints.medium}) {
+    flex: 0;
+    padding: 1em 3%;
+    font-size: 1em;
+    background: #666666;
+    color: ${props => props.theme.color.white};
+  }
 `
 
 const Description = styled.div`
   flex: 1;
-  padding: 4em 3em 0;
-  font-size: 1.2em;
+  padding: 10em 3em 0;
   text-align: center;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    padding: 0 5%;
+    font-size: 0.8em;
+    background: #666666;
+  }
 `
 
 const Logo = styled.img`
   height: 6em;
   margin: 1em 0;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    display: none;
+  }
 `
 
 const Button = styled.button`
@@ -123,8 +197,17 @@ const Button = styled.button`
   width: 6em;
   border: 0;
   background: ${props => props.theme.color.lightBlue};
-  color: inherit;
+  color: ${props => props.theme.color.white};
   cursor: pointer;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    height: 2rem;
+    width: 2rem;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    font-size: 0.4em;
+  }
 `
 
 const pictures = [
@@ -143,8 +226,14 @@ const pictures = [
   }
 ]
 
-const Products = () => (
+const Products = (props) => (
   <Container>
+    <Square>
+      <Arrow className="fas fa-angle-left" onClick={() => props.changeModal(false)}/>
+      <SquarePicture src={icon}/>
+      <SquareText>Productos</SquareText>
+      <Arrow className="fas fa-angle-right" onClick={() => props.changeModal(true)} right/>
+    </Square>
     <PhotoGrid>
       {pictures.map((pic) => (
         <Photo image={pic.url}>
@@ -153,12 +242,9 @@ const Products = () => (
           </OpaqueDiv>
         </Photo>
       ))}
+      <Button><i className="fas fa-shopping-cart fa-3x"/></Button>
     </PhotoGrid>
     <RightContainer>
-      <Square>
-        <SquarePicture src={icon}/>
-        <SquareText>Productos</SquareText>
-      </Square>
       <Description>
         <p>
           Al comprar uno de estos productos estás apoyando a Te tempor exquisitaque an voluptate fore illum ubi tempor.
@@ -168,7 +254,6 @@ const Products = () => (
         <Logo src={logo}/>
       </Description>
     </RightContainer>
-    <Button><i className="fas fa-shopping-cart fa-3x"/></Button>
   </Container>
 );
 export default Products;
