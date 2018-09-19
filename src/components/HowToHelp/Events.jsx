ae7015451863d35307804b5ae9bd58f2a3751dcf
Daniel Hernandez <tamdannyhdz@gmail.com>
2018-09-19 18:33:43 -0500

    Add responsiveness to Events modal

--- src/components/HowToHelp/Events.jsx
+++ src/components/HowToHelp/Events.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import breakpoints from '../../utils/breakpoints';
 import backgroundImage from './assets/events-picture.png';
 import icon from './assets/events-logo.svg';
 import casaMusaLogo from './assets/casa-musa-logo.svg';
@@ -10,11 +11,18 @@ const Container = styled.div`
   position: relative;
   height: 100%;
   width: 100%;
-  box-sizing: border-box;
-  padding: 8em 4em 4em 6em;
   background: url(${backgroundImage}) no-repeat right center;
   background-size: cover;
   color: ${props => props.theme.color.white};
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    flex-direction: column;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 5em 0 0;
+    background: ${props => props.theme.color.white};
+    color: ${props => props.theme.color.black};
+  }
 `
 
 const Square = styled.div`
@@ -25,12 +33,42 @@ const Square = styled.div`
   width: 10em;
   text-align: center;
   background: ${props => props.theme.color.lightBlue};
+  color: ${props => props.theme.color.white};
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    position: relative;
+    top: 0;
+    left: 0;
+    height: 6em;
+    width: 6em;
+  }
 `
 
 const SquarePicture = styled.img`
   max-height: 4em;
   max-width: 6em;
   padding: 1.5em;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    max-height: 2em;
+    max-width: 3em;
+    padding: 1em;
+  }
+`
+
+const Arrow = styled.i`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: ${props => !props.right ? '-1em' : 'initial'};
+  right: ${props => props.right ? '-1em' : 'initial'};
+  font-size: 3em;
+  color: ${props => props.theme.color.black};
+  cursor: pointer;
+
+  @media screen and (min-width: ${breakpoints.medium}) {
+    display: none !important;
+  }
 `
 
 const SquareText = styled.p`
@@ -38,6 +76,28 @@ const SquareText = styled.p`
   bottom: 0.5em;
   width: 100%;
   font-size: 1.3em;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    font-size: 1em;
+  }
+`
+
+const Description = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 8em 4em 4em 6em;
+  color: ${props => props.theme.color.white};
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    height: initial;
+    box-sizing: border-box;
+    padding: 2em 3%;
+    margin: 2em 0;
+    background: url(${backgroundImage}) no-repeat right center;
+    background-size: cover;
+  }
 `
 
 const Info = styled.div`
@@ -46,6 +106,11 @@ const Info = styled.div`
   padding: 4.5em 2em 0;
   flex-direction: column;
   font-size: 1.5em;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    flex: 1;
+    font-size: 1em;
+  }
 `
 
 const Header = styled.h3`
@@ -63,6 +128,10 @@ const SmallText = Paragraph.extend`
 
 const Logo = styled.img`
   padding: 1em;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+
+  }
 `
 
 const VerticalLine = styled.div`
@@ -71,24 +140,28 @@ const VerticalLine = styled.div`
   background: ${props => props.theme.color.lightBlue};
 `
 
-const Events = () => (
+const Events = (props) => (
   <Container>
     <Square>
+      <Arrow className="fas fa-angle-left" onClick={() => props.changeModal(false)}/>
       <SquarePicture src={icon}/>
       <SquareText>Eventos</SquareText>
+      <Arrow className="fas fa-angle-right" onClick={() => props.changeModal(true)} right/>
     </Square>
-    <Info flex='1'>
-      <Header>Obra de teatro</Header>
-      <Paragraph>Ciclo de teatro en colaboración con casa Musa</Paragraph>
-      <SmallText>* Fechas por confirmar *</SmallText>
-      <Logo src={casaMusaLogo}/>
-    </Info>
-    <VerticalLine/>
-    <Info flex='1.4'>
-      <Header>Conferencia</Header>
-      <Paragraph>Guadalupe Nettel</Paragraph>
-      <SmallText>* Fechas por confirmar *</SmallText>
-    </Info>
+    <Description>
+      <Info flex='1'>
+        <Header>Obra de teatro</Header>
+        <Paragraph>Ciclo de teatro en colaboración con casa Musa</Paragraph>
+        <SmallText>* Fechas por confirmar *</SmallText>
+        <Logo src={casaMusaLogo}/>
+      </Info>
+      <VerticalLine/>
+      <Info flex='1.4'>
+        <Header>Conferencia</Header>
+        <Paragraph>Guadalupe Nettel</Paragraph>
+        <SmallText>* Fechas por confirmar *</SmallText>
+      </Info>
+    </Description>
   </Container>
 );
 
